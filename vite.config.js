77dac746bd6acda1d04ff6a06ae064921be9e561
Brendan Murphy <emailbmurph@gmail.com>
2023-07-11 09:52:28 -0400

    remove unused files, update build config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        //manualChunks: false,
+        inlineDynamicImports: true,
+        //entryFileNames: '[name].js',   // currently does not work for the legacy bundle
+        //assetFileNames: '[name].[ext]', // currently does not work for images
+      },
+    }
+  },
 })
